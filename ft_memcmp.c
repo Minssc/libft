38b8d707c3de94f8162da9d94273369d87a94882
Minsung Kim <minsunki@c1r6s4.42seoul.kr>
2021-03-04 02:12:39 +0900

    minor update

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,7 +6,7 @@
 /*   By: minsunki <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/12 13:04:06 by minsunki          #+#    #+#             */
-/*   Updated: 2021/03/03 20:29:33 by minsunki         ###   ########.fr       */
+/*   Updated: 2021/03/04 02:04:55 by minsunki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,14 @@
 
 int		ft_memcmp(const void *s1, const void *s2, size_t n)
 {
-	unsigned char *sp1;
-	unsigned char *sp2;
+	unsigned char	*sp1;
+	unsigned char	*sp2;
+	int				ret;
 
 	sp1 = (unsigned char *)s1;
 	sp2 = (unsigned char *)s2;
 	while (n--)
-		if (*sp1++ != *sp2++)
-			return (*(sp1 - 1) - *(sp2 - 1));
+		if ((ret = (*sp1++ - *sp2++)))
+			return (ret);
 	return (0);
 }
