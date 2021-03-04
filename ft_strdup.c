6bf0cfa0ed22e1ad8416d7441140ac2cc2df0f09
Minsung Kim <minsunki@c1r6s4.42seoul.kr>
2021-03-04 22:36:06 +0900

    strdup cleanup

--- ft_strdup.c
+++ ft_strdup.c
@@ -6,7 +6,7 @@
 /*   By: minsunki <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/19 13:10:54 by minsunki          #+#    #+#             */
-/*   Updated: 2021/03/03 20:30:59 by minsunki         ###   ########.fr       */
+/*   Updated: 2021/03/04 22:30:18 by minsunki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,17 +14,12 @@
 
 char	*ft_strdup(const char *str)
 {
-	int		i;
+	size_t	slen;
 	char	*nstr;
 
-	if (!(nstr = (char *)malloc(sizeof(char) * (ft_strlen(str) + 1))))
+	slen = ft_strlen(str);
+	if (!(nstr = (char *)malloc(sizeof(char) * (slen + 1))))
 		return (0);
-	i = 0;
-	while (str[i])
-	{
-		nstr[i] = str[i];
-		i++;
-	}
-	nstr[i] = 0;
+	ft_strlcpy(nstr, str, slen +1);
 	return (nstr);
 }
