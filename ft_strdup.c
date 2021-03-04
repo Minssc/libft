46e26afdd82170984ac21d755a556ae5b21147e7
Minsung Kim <minsunki@c1r6s4.42seoul.kr>
2021-03-04 23:06:38 +0900

    strdup norm fix

--- ft_strdup.c
+++ ft_strdup.c
@@ -6,7 +6,7 @@
 /*   By: minsunki <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/19 13:10:54 by minsunki          #+#    #+#             */
-/*   Updated: 2021/03/04 22:30:18 by minsunki         ###   ########.fr       */
+/*   Updated: 2021/03/04 23:06:15 by minsunki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,6 @@ char	*ft_strdup(const char *str)
 	slen = ft_strlen(str);
 	if (!(nstr = (char *)malloc(sizeof(char) * (slen + 1))))
 		return (0);
-	ft_strlcpy(nstr, str, slen +1);
+	ft_strlcpy(nstr, str, slen + 1);
 	return (nstr);
 }
