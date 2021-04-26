d6f4fa4a8a20ec3dfcbb3840d5893df0e4a54160
minssc (minsunki) <pauaup@naver.com>
2021-04-26 15:41:49 +0900

    ft_putstrn_fd.c cleanup

--- ft_putstrn_fd.c
+++ ft_putstrn_fd.c
@@ -6,7 +6,7 @@
 /*   By: minsunki <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/24 14:25:59 by minsunki          #+#    #+#             */
-/*   Updated: 2021/03/24 14:30:00 by minsunki         ###   ########.fr       */
+/*   Updated: 2021/04/03 00:30:38 by minsunki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,5 @@ void	ft_putstrn_fd(char *s, int fd, size_t len)
 	if (!s || fd < 0)
 		return ;
 	slen = ft_strlen(s);
-	len = (slen < len ? slen : len);
-	write(fd, s, len);
+	write(fd, s, (slen < len ? slen : len));
 }
